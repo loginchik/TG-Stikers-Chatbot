1d39871661e09743ac69d34b003b2fdae847670b
Anna Loginova <zv1376a@gmail.com>
2023-07-29 17:34:33 +0300

    Logging error fixed

--- data.py
+++ data.py
@@ -1,12 +1,34 @@
 import json
-import logging
+import os 
 
-logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s')
-
-with open('data/sets.json') as sets_file:
-    try: 
-        sets = json.load(sets_file)
-        logging.info('Loaded json')
-    except json.decoder.JSONDecodeError:
-        logging.error('JSON sets load error')
+from log import info_logger, error_logger
+        
+            
+def load_extisting_sets(path_to_file: str = os.path.relpath('data/sets.json')) -> dict | None:
+    try:
+        # Open the source file in reading mode 
+        source_file = open(file=path_to_file, mode='r')
+        info_logger.info(f'{path_to_file} is opened')
+        try: 
+            # Load json data into python dict 
+            sets = json.load(source_file)
+            info_logger.info(f'Loaded {path_to_file}')
+            
+            # Close the connection to source file 
+            source_file.close()
+            info_logger.info(f'{path_to_file} is closed')
+            
+            # Return gathered info
+            return sets
+        # In case of decoder error 
+        # Error message is logged, None is returned by function 
+        except json.decoder.JSONDecodeError as e:
+            error_logger.error(f'Decode of {path_to_file} error')
+            return None
+    # In case file is not found, error is logged, None is returned as a restult of function
+    except FileNotFoundError:
+        error_logger.error(f'{path_to_file} is not found')
+        return None
     
+
+print(load_extisting_sets())
